a8b424a14b70ca97da8bb714ddd6d71458e08e9f
andreynikolaevtyumen <109260033+andreynikolaevtyumen@users.noreply.github.com>
2022-10-17 18:09:08 +0500

    Small fix

--- task010.py
+++ task010.py
@@ -7,11 +7,3 @@ lst = [random.randint(0, 10) for i in range(random.randint(5,20))]
 print(f'Исходный список:\n {lst}')
 random.shuffle(lst)
 print(f'Перемещанный сеисок: \n{lst}')
-
-
-
-
-# from time import time
-# my_time = time()
-# my_time %= 1
-# print(int((my_time % 1)*100))
