19afaa1a46e174c00e3fb2fb7773013f054ee2ee
andreynikolaevtyumen <109260033+andreynikolaevtyumen@users.noreply.github.com>
2022-10-17 18:02:46 +0500

    Initial commit

---
+++ task007.py
@@ -0,0 +1,21 @@
+
+# Напишите программу, которая принимает на вход число N и выдает набор произведений 
+# чисел от 1 до N.
+# Пример:
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+import os
+os.system('cls')
+
+def factorial (number, count = 1):
+    for i in range(1, number + 1):
+        count *= i
+    return count
+
+n = int(input('Введите число: '))
+print(f'Набор произведений чисел от 1 до {n} = ', end = '')
+for i in range(1, n + 1):
+    if i == n: 
+        print(f'{factorial(i)}')
+    else:
+        print(f'{factorial(i)}', end = ', ')
